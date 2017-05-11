369ed19ba43a248a667c99749799d4bf4f9af577
hayakawa <hayakawatakahiko@gmail.com>
2017-05-12 00:30:24 +0900

    h2をサーバー起動時に立ち上げる設定、それを利用するようにする設定 http://localhost:8080/SpringBootMybatisH2/console/ sa saでログイン可能

--- SpringBootMybatisH2/src/main/java/com/example/SpringBootMybatisH2Application.java
+++ SpringBootMybatisH2/src/main/java/com/example/SpringBootMybatisH2Application.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
 public class SpringBootMybatisH2Application {
 
 	public static void main(String[] args) {
---
+++ SpringBootMybatisH2/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+
+insert into TB_TESTDATA (ID, NAME) values (1, 'test');
+insert into TB_TESTDATA (ID, NAME) values (2, 'aaa');
+insert into TB_TESTDATA (ID, NAME) values (3, 'bbb');
