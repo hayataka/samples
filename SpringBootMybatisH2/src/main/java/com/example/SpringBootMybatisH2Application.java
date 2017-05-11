b38ea0b8cccd84c8b2e327152346b0ee6f5ccc24
hayakawa <hayakawatakahiko@gmail.com>
2017-05-12 00:00:28 +0900

    プロジェクト作成初期インポート

---
+++ SpringBootMybatisH2/src/main/java/com/example/SpringBootMybatisH2Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisH2Application.class, args);
+	}
+}
