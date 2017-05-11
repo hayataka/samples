b079a66d32d4884e8406a7ea2d6451701f694760
hayakawa <hayakawatakahiko@gmail.com>
2017-05-12 00:44:56 +0900

    mapper scan設定の追加、既存のcontrollerからmapperの呼び出し

--- SpringBootMybatisH2/src/main/java/com/example/SpringBootMybatisH2Application.java
+++ SpringBootMybatisH2/src/main/java/com/example/SpringBootMybatisH2Application.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+
 @SpringBootApplication
 @ImportResource("classpath:applicationContext.xml")
+@MapperScan("com.example.mapper")
 public class SpringBootMybatisH2Application {
 
 	public static void main(String[] args) {
--- SpringBootMybatisH2/src/main/java/com/example/controller/RootController.java
+++ SpringBootMybatisH2/src/main/java/com/example/controller/RootController.java
@@ -1,17 +1,27 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.form.RootForm;
+import com.example.mapper.TbTestdataMapper;
+import com.example.model.TbTestdata;
 
 @Controller
 @RequestMapping("/")
 public class RootController {
 
+	@Autowired
+	private TbTestdataMapper mapper;
+	
+	@Transactional
 	@RequestMapping("/")
 	public String test(RootForm rootForm) {
+		TbTestdata entity = mapper.selectByPrimaryKey(1);
 		rootForm.setId(30);
+		rootForm.setName(entity.getName());
 		return "index";
 	}
 
