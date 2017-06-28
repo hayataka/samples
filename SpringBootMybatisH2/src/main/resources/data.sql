5bcd72470dae6c1be488fca7afd94ef808bd888e
hayakawa <hayakawatakahiko@gmail.com>
2017-06-28 21:17:55 +0900

    table変更

--- SpringBootMybatisH2/src/main/resources/data.sql
+++ SpringBootMybatisH2/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 
-insert into TB_TESTDATA (ID, NAME) values (1, 'test');
-insert into TB_TESTDATA (ID, NAME) values (2, 'aaa');
-insert into TB_TESTDATA (ID, NAME) values (3, 'bbb');
+ insert into TB_TESTDATA (ID, NAME) values (1, '太郎');
+ insert into TB_TESTDATA (ID, NAME) values (2, 'aaa');
+ insert into TB_TESTDATA (ID, NAME) values (3, 'bbb');
