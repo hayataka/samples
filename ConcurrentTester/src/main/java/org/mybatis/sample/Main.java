3430e321b882fb822269aafe24965f261bbc68fb
hayakawa <hayakawatakahiko@gmail.com>
2018-04-06 22:52:03 +0900

    sql実行箇所のシンプル化

--- ConcurrentTester/src/main/java/org/mybatis/sample/Main.java
+++ ConcurrentTester/src/main/java/org/mybatis/sample/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -11,30 +12,42 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatis.sample.mapper.SampleMapper;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
+		Main me = new Main();
+		me.process();
+	}	
 
-		SqlSessionFactory factory = null;
-		// 設定ファイルの読み込み
+	private SqlSessionFactory factory = null;
+	
+	public void process() throws IOException {
+		factory = null;
+		// 設定ファイルの読み込み一度だけ
 		try (InputStream in = Main.class.getResourceAsStream("/mybatis-config.xml")) {
 			// applicationScopeで使う
 			factory = new SqlSessionFactoryBuilder().build(in);
 		}
 
-		boolean autoCommit = true;
-		try (SqlSession session = factory.openSession(autoCommit)) {
-			// ★SqlSession を使って SQL を実行する
-			// List<Map<String, Object>> result =
-			// session.selectList("org.mybatis.mapper.SampleMapper.selectTest");
+		doSql(session ->{
 			SampleMapper mapper = session.getMapper(SampleMapper.class);
 			List<Map<String, Object>> result = mapper.selectTest();
-
+			System.out.println(session.getConnection());
 			result.forEach(row -> {
-				System.out.println("---------------");
 				row.forEach((columnName, value) -> {
-					System.out.printf("columnName=%s, value=%s%n", columnName, value);
+	//				System.out.printf("columnName=%s, value=%s%n", columnName, value);
 				});
 			});
-		}
+		});
+	}	
+	
+	public void doSql(Consumer<SqlSession> sql) {
+		boolean autoCommit = true;
+		try (SqlSession session = factory.openSession(autoCommit)) {
+			sql.accept(session);
+		}		
 	}
+	
+	
+
+	
+	
 }
--- ConcurrentTester/src/main/java/org/mybatis/sample/mapper/SampleMapper.java
+++ ConcurrentTester/src/main/java/org/mybatis/sample/mapper/SampleMapper.java
@@ -3,6 +3,7 @@ package org.mybatis.sample.mapper;
 import java.util.List;
 import java.util.Map;
 
+
 public interface SampleMapper {
 
 	List<Map<String, Object>> selectTest();
